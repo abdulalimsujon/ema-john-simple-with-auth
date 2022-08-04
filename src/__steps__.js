6cf56747305a02d94da4471b95740d0a39054d57
abdulalimsujon <alimsujon12@gmail.com>
2022-08-04 18:03:46 +0600

    intial firebase setup

---
+++ src/__steps__.js
@@ -0,0 +1,14 @@
+/**
+ * 1.create a project in console . google.com
+ * 2.npm install firebase
+ * 3.import firebase.init.js and import getAuth 
+ * 4.firebase setting<Authentication<enable email and password 
+ * 5.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
